90b36d1eeb89064952cd34a514b5283ec08c7e23
pcaldi <pcaldi@gmail.com>
2024-08-08 18:25:35 -0300

    configurando theme na aplicação

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
+import { Home } from './pages/Home'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
+  </StrictMode>,
+)
