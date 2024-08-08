49899c66c40dc09d0a1ddee3b851a8bb61caf638
pcaldi <pcaldi@gmail.com>
2024-08-08 18:25:05 -0300

    configurando estilo global e theme na aplicação

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    GRAY_900: "#121214",
+    GRAY_800: "#202024",
+    GRAY_700: "#3E3B47",
+    GRAY_600: "#5A595E",
+
+    WHITE: "#F4EDE8",
+
+    BRAND_600: "#2833ff",
+    BRAND_500: "#98e1fc",
+  }
+};
