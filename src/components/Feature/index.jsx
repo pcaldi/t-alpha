54c10f5a6d35d673760a5dad08ee50ac9385831c
pcaldi <pcaldi@gmail.com>
2024-08-09 10:01:36 -0300

    componente feature

---
+++ src/components/Feature/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { Container } from './styles';
+
+export function Feature({ icon: Icon, title, ...rest }) {
+  return (
+    <Container {...rest}>
+      {Icon && <Icon size={42} />}
+
+      <footer>
+        {title}
+      </footer>
+    </Container>
+  );
+};
---
+++ src/components/Feature/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 200px;
+  height: 200px;
+
+  background-color: ${({ theme }) => theme.COLORS.BRAND_500};
+  color: ${({ theme }) => theme.COLORS.GRAY_900};
+  border-radius: 7px;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+
+  > svg {
+    flex: 1;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > footer {
+    background-color: ${({ theme }) => theme.COLORS.BRAND_600};
+    width: 100%;
+    padding: 24px 0;
+    text-align: center;
+    font-weight: 700;
+  }
+`;
