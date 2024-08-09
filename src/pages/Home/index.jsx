34038c2e009258de7d8d6958159022ccf85c7f21
pcaldi <pcaldi@gmail.com>
2024-08-09 12:50:03 -0300

    page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,18 @@
+import { FiTag, FiEdit, FiClipboard } from 'react-icons/fi';
 import { Container } from "./styles"
+
+import { Header } from "../../components/Header"
+import { Feature } from "../../components/Feature"
 
 export function Home() {
   return (
     <Container>
-      <h1>Home</h1>
+      <Header />
+      <main>
+        <Feature title="Produtos" icon={FiTag} to="/product" />
+        <Feature title="Criar" icon={FiClipboard} to="/new-product" />
+        <Feature title="Editar" icon={FiEdit} to="/update-product/:id" />
+      </main>
     </Container>
   )
 }
