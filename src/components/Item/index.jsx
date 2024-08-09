eeb61a0ff3cec855b9bd7fa3b10f68a7fe0dd357
pcaldi <pcaldi@gmail.com>
2024-08-09 15:37:56 -0300

    refatorando component item

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,22 +4,19 @@ import { Container } from './styles'
 export function Item({ data, ...rest }) {
   return (
     <Container {...rest}>
-      {
-        data.item &&
-        <main>
-          {
-            data.item.map((item) => (
-              <section key={String(item.id)}>
 
-                <h2>Produto: {item.name}</h2>
-                <p><strong>Descrição:</strong> {item.description}</p>
-                <p><strong>Preço:</strong> {item.price}</p>
-                <p><strong>Estoque:</strong> {item.stock}</p>
-              </section>
-            ))
-          }
-        </main>
-      }
+      <main>
+
+        <section>
+
+          <h2>Produto: {data.name}</h2>
+          <p><strong>Descrição:</strong> {data.description}</p>
+          <p><strong>Preço: R$</strong> {data.price}</p>
+          <p><strong>Estoque:</strong> {data.stock}</p>
+        </section>
+
+      </main>
+
 
     </Container>
   )
