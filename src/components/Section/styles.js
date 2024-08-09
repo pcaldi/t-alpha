da1c8ecdbb4073c772c26b00417c9f7f9eeed145
pcaldi <pcaldi@gmail.com>
2024-08-09 17:01:24 -0300

    component section

---
+++ src/components/Section/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+
+  > h2 {
+    font-size: 20px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    padding-bottom: 12px;
+
+  }
+
+
+`;
