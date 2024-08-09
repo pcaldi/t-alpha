17fa796b52943bf73d436174b51e5df09eb4f5a3
pcaldi <pcaldi@gmail.com>
2024-08-09 13:13:42 -0300

    redirecionando a rota

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 
-/* import { AppRoutes } from "./app.routes"; */
+import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
+import { useAuth } from "../hooks/auth"
+
 export function Routes() {
+
+  const { token } = useAuth()
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {token ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
