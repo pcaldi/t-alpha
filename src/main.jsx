1341b1b64fd540bdaa3ba9371d80e80722fad503
pcaldi <pcaldi@gmail.com>
2024-08-09 12:50:15 -0300

    adicionando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 
+import { ThemeProvider } from 'styled-components'
 
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
-import { Home } from './pages/Home'
+import { Routes } from './routes'
+
+import { AuthProvider } from './hooks/auth'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
