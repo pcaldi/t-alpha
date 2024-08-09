f412b2b92e9005963a098cce113a736bc5234afc
pcaldi <pcaldi@gmail.com>
2024-08-09 16:13:56 -0300

    recebendo a nota com o params e exibindo o produto

--- src/pages/UpdateProduct/index.jsx
+++ src/pages/UpdateProduct/index.jsx
@@ -1,20 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { api } from "../../services/api";
+
 import { Container, Header, Form } from "./styles";
 
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
 import { TextArea } from "../../components/TextArea"
-import { useNavigate } from "react-router-dom";
 
 
 export function UpdateProduct() {
+  const [data, setData] = useState(null)
 
-
+  const params = useParams()
   const navigate = useNavigate()
 
   function handleBack() {
     navigate(-1)
   }
 
+  useEffect(() => {
+    async function fetchProduct() {
+      const response = await api.get(`/api/products/get-one-product/${params.id}`)
+      setData(response.data.data.product)
+    }
+
+    fetchProduct()
+  }, [])
 
   return (
     <Container>
@@ -22,18 +36,22 @@ export function UpdateProduct() {
         <h1>Editar</h1>
         <nav>
           <Button title="Voltar" onClick={handleBack} />
+          <Button title="Excluir" onClick={handleBack} />
         </nav>
       </Header>
+      {
 
-      <Form>
-        <Input placeholder="Produto" />
-        <TextArea placeholder="Descrição" />
-        <Input placeholder="Preço" />
-        <Input placeholder="Quantidade" />
+        data &&
+        <Form>
+          <Input value={data.name} />
+          <TextArea value={data.description} />
+          <Input value={data.price} />
+          <Input value={data.stock} />
 
-        <Button title="Editar" />
+          <Button title="Salvar" />
 
-      </Form>
+        </Form>
+      }
     </Container>
   )
 }
